881ff42743764249492b40820310a72d23f56613
Tomasz Jagas <tomasz.jagas@gmail.com>
2020-12-01 22:29:41 +0100

    Add project files.

---
+++ c-programming-015-InitializeAllArrayPlay/main.c
@@ -0,0 +1,20 @@
+/**********************************************************
+* Description: Some play with initialization of whole array
+***********************************************************/
+
+#include <stdio.h>
+
+int main() {
+	int arrayValues[10] = { 0,1,4,9,16 };
+	int i;
+
+	for (i = 5; i < 10; ++i) {
+		arrayValues[i] = i * i;
+	}
+
+	for (i = 0; i < 10; ++i) {
+		printf("arrayValues[%i] = %i\n", i, arrayValues[i]);
+	}
+
+	return 0;
+}
